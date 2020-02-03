aa45c4a268cca7bd9eccb9c83f19ac23a6497801
Shamis Asghar <shamis.asghar@outlook.com>
2020-02-03 17:51:59 +0500

    chat message changed with userid

--- app/src/main/java/com/hypernym/evaconnect/models/UserDetails.java
+++ app/src/main/java/com/hypernym/evaconnect/models/UserDetails.java
@@ -2,6 +2,7 @@ package com.hypernym.evaconnect.models;
 
 public class UserDetails {
     public static String username = "";
+    public static int userid ;
     public static String password = "";
     public static String chatWith = "";
     public static String email = "";
